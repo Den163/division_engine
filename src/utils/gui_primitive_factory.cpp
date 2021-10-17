69a270f42c640a2270566fef564d263891ccb9cd
Den163 <abramov.denis163@gmail.com>
2021-10-17 11:42:54 +0300

    Added create quad primitive func

--- src/utils/gui_primitive_factory.cpp
+++ src/utils/gui_primitive_factory.cpp
@@ -27,25 +27,50 @@ std::tuple<const entt::entity&, GuiMesh&> GuiPrimitiveFactory::createMeshEntity(
 std::tuple<const entt::entity&, GuiMesh&> GuiPrimitiveFactory::createTriangle(
     entt::registry& ecsRegistry,
     const Transform& transform,
-    const std::array<glm::vec3, 3>& vertexPositions,
+    const std::array<glm::vec3, 3>& vertices,
     const glm::vec4& color)
 {
-    std::array<GuiVertex, 3> vertices {};
-    for (auto i = 0; i < vertices.size(); i++) {
-        vertices[i] = GuiVertex { vertexPositions[i], color };
+    std::array<GuiVertex, 3> guiVertices {};
+    for (auto i = 0; i < vertices.size(); i++)
+    {
+        guiVertices[i] = GuiVertex { vertices[i], color };
     }
 
-    return createTriangle(ecsRegistry, transform, GuiTriangle {vertices});
+    return createTriangle(ecsRegistry, transform, GuiTriangle {guiVertices});
 }
 
 std::tuple<const entt::entity&, GuiMesh&>
 GuiPrimitiveFactory::createTriangle(entt::registry& ecsRegistry, const Transform& transform, const GuiTriangle& triangle)
 {
     auto [e, mesh] = createMeshEntity(ecsRegistry, transform.position, transform.rotation, transform.scale);
-
     mesh.renderShape = RenderMode::Triangles;
     mesh.vertices = std::vector<GuiVertex> { triangle.vertices.begin(), triangle.vertices.end() };
 
     return {e, mesh};
+}
+
+std::tuple<const entt::entity&, GuiMesh&> GuiPrimitiveFactory::createQuad(
+    entt::registry& guiRegistry,
+    const Transform& transform,
+    const std::array<glm::vec3, 4>& vertices,
+    const glm::vec4& color)
+{
+    std::array<GuiVertex, 4> guiVertices {};
+    for (size_t i = 0; i < vertices.size(); i++)
+    {
+        guiVertices[i] = { vertices[i], color };
+    }
+
+    return createQuad(guiRegistry, transform, GuiQuad {guiVertices});
+}
+
+std::tuple<const entt::entity&, GuiMesh&>
+GuiPrimitiveFactory::createQuad(entt::registry& guiRegistry, const Transform& transform, const GuiQuad& quad)
+{
+    auto [e, mesh] = createMeshEntity(guiRegistry, transform.position, transform.rotation, transform.scale);
+    mesh.renderShape = RenderMode::TrianglesStrip;
+    mesh.vertices = std::vector<GuiVertex> { quad.vertices.begin(), quad.vertices.end() };
+
+    return { e, mesh };
 }
 
