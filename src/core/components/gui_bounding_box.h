af9b293ead2f35a013caded3f713301e14a6d178
Den163 <abramov.denis163@gmail.com>
2021-11-03 21:33:30 +0300

    Reverted some unused changes

--- src/core/components/gui_bounding_box.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glm/vec2.hpp>
-
-struct GuiBoundingBox
-{
-    glm::vec2 center;
-    glm::vec2 size;
-};
--- src/core/components/gui_rect.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glm/vec2.hpp>
-
-struct GuiRect
-{
-    glm::vec2 position;
-    glm::vec2 size;
-};
